aaaccb2c451afa6c50b2614815be79c8ceb64e26
julio <32973371+juliogdomingues@users.noreply.github.com>
2023-12-06 20:00:16 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
 const { default: loadCustomRoutes } = require("next/dist/lib/load-custom-routes")
 
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({chave : "alunos do curso.dev são pessoas acima da média"});
 }
 
