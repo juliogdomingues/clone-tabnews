aa9b81a716b64db4e1ae198896da99eb8a32aec5
julio <32973371+juliogdomingues@users.noreply.github.com>
2023-12-07 14:21:15 +0000

    db version test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,24 @@ const { default: loadCustomRoutes } = require("next/dist/lib/load-custom-routes"
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({chave : "alunos do curso.dev são pessoas acima da média"});
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  // const max_connections =
+  // const used_connectins = 
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      }
+    }
+    // database_version_result: ,
+    // max_connections: ,
+    // used_connectins: ,
+  });
 }
 
 export default status
