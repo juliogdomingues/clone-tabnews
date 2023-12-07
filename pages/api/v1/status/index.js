821595dad5531525f53afca5f693328d4e1d62da
julio <32973371+juliogdomingues@users.noreply.github.com>
2023-12-07 13:34:07 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 const { default: loadCustomRoutes } = require("next/dist/lib/load-custom-routes")
 
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
