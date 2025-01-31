990645f921ea383d303444c076d2606f0a099212
julio <32973371+juliogdomingues@users.noreply.github.com>
2025-01-31 13:56:45 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,26 +1,35 @@
-const { default: loadCustomRoutes } = require("next/dist/lib/load-custom-routes")
+const {
+  default: loadCustomRoutes,
+} = require("next/dist/lib/load-custom-routes");
 
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-
-  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  // const max_connections =
-  // const used_connectins = 
-
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
-      }
-    }
-    // database_version_result: ,
-    // max_connections: ,
-    // used_connectins: ,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
-export default status
+export default status;
